6f25800352c05001186400f3bab5865e0f8c3015
karinevitoria <karinevfalves@gmail.com>
2022-05-21 16:15:03 -0300

    criacao do routes, pagina de cadastro

--- assets/js/login-register.js
+++ assets/js/login-register.js
@@ -12,7 +12,7 @@ function showRegisterForm(){
         $('.login-footer').fadeOut('fast',function(){
             $('.register-footer').fadeIn('fast');
         });
-        $('.modal-title').html('Register with');
+        $('.modal-title').html('Registrar com');
     }); 
     $('.error').removeClass('alert alert-danger').html('');
        
@@ -24,7 +24,7 @@ function showLoginForm(){
             $('.login-footer').fadeIn('fast');    
         });
         
-        $('.modal-title').html('Login with');
+        $('.modal-title').html('Entrar com');
     });       
      $('.error').removeClass('alert alert-danger').html(''); 
 }
@@ -61,7 +61,7 @@ function loginAjax(){
 
 function shakeModal(){
     $('#loginModal .modal-dialog').addClass('shake');
-             $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+             $('.error').addClass('alert alert-danger').html("Email e senha não conferem");
              $('input[type="password"]').val('');
              setTimeout( function(){ 
                 $('#loginModal .modal-dialog').removeClass('shake'); 
