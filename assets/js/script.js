50b7ac60fc3f08d39f4d0b2700dae71bdc23ae68
karinevitoria <karinevfalves@gmail.com>
2022-05-21 18:59:52 -0300

    função pegar cep

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,23 @@ searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   alert(inputValue);
 });
+
+function consultarCepAjax() {
+    var cep = $('#cep').val();
+    $.getJSON(`https://viacep.com.br/ws/${cep}/json/`, function (data, status) {
+  
+      try {
+  
+        if (status == 'success') {
+          var dadosCep = data
+  
+          $('#localidade').val(dadosCep.localidade)
+          $('#bairro').val(dadosCep.bairro)
+          $('#uf').val(dadosCep.uf)
+          $('#logradouro').val(dadosCep.logradouro)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    });
+  }
